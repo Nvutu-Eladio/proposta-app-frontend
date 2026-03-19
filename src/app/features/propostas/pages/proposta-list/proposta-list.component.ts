ce05efddaea4e0734175232912d73d16fb2b0e54
Nvutu-Eladio <nveladio@gmail.com>
2026-03-18 21:28:48 -0300

    feat: ajustando integracao com backend

--- src/app/features/propostas/pages/proposta-list/proposta-list.component.ts
+++ src/app/features/propostas/pages/proposta-list/proposta-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
 import { PropostaService } from '../../../../core/services/proposta.service';
 import { PropostaResponse } from '../../../../core/models/proposta.model';
 
@@ -8,29 +8,38 @@ import { PropostaResponse } from '../../../../core/models/proposta.model';
   styleUrls: ['./proposta-list.component.scss'],
   standalone: false,
 })
-export class PropostaListComponent implements OnInit {
+export class PropostaListComponent implements OnInit, OnDestroy {
   propostas: PropostaResponse[] = [];
-  isLoading = false;
   errorMessage = '';
+  private refreshInterval: ReturnType<typeof setInterval> | null = null;
 
-  constructor(private propostaService: PropostaService) {}
+  constructor(
+    private propostaService: PropostaService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.carregarPropostas();
+    this.refreshInterval = setInterval(() => this.carregarPropostas(), 5000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.refreshInterval) {
+      clearInterval(this.refreshInterval);
+    }
   }
 
   carregarPropostas(): void {
-    this.isLoading = true;
     this.errorMessage = '';
 
     this.propostaService.listarPropostas().subscribe({
       next: (data) => {
         this.propostas = data;
-        this.isLoading = false;
+        this.cdr.markForCheck();
       },
       error: (err: Error) => {
         this.errorMessage = err.message;
-        this.isLoading = false;
+        this.cdr.markForCheck();
       },
     });
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: '/proposta',
+  apiUrl: 'http://localhost:8080/proposta',
 };
